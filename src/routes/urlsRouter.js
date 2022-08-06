da1a69360579e5b6714fe0fc90c2289e5a781a2d
Januacele <januacelevieira@hotmail.com>
2022-08-06 00:57:52 -0300

    Import controller shortenUrl at rout urls/shorten

--- src/routes/urlsRouter.js
+++ src/routes/urlsRouter.js
@@ -1,12 +1,12 @@
 import {Router} from 'express';
-import {  } from '../controllers/urlsController.js';
+import { shortenUrl } from '../controllers/urlsController.js';
 import { checkToken, checkUrlSchema } from '../middlewares/urlsMiddleware.js';
 
 
 const urlsRouter = Router();
 
 
-urlsRouter.post("/urls/shorten", checkToken, checkUrlSchema );
+urlsRouter.post("/urls/shorten", checkToken, checkUrlSchema, shortenUrl );
 
 
 export default urlsRouter;
