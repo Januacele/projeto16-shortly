8c6d142fe9a60f784575e517ff081f7921f4d1b2
Januacele <januacelevieira@hotmail.com>
2022-08-07 14:15:26 -0300

    Add timeout library
    Trying to solve h12 error

--- src/routes/urlsRouter.js
+++ src/routes/urlsRouter.js
@@ -1,12 +1,12 @@
 import {Router} from 'express';
 import { shortenUrl } from '../controllers/urlsController.js';
 import { checkToken, checkUrlSchema } from '../middlewares/urlsMiddleware.js';
-
+import {timeout} from 'connect-timeout';
 
 const urlsRouter = Router();
 
 
-urlsRouter.post("/urls/shorten", checkToken, checkUrlSchema, shortenUrl );
+urlsRouter.post("/urls/shorten", timeout('5s'), checkToken, checkUrlSchema, shortenUrl);
 
 
 export default urlsRouter;
