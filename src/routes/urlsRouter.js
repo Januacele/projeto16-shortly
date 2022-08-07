1bb196bde7555f3bf2b9560ee2cb87019112a268
Januacele <januacelevieira@hotmail.com>
2022-08-07 15:17:59 -0300

    trying to fix h10 error

--- src/routes/urlsRouter.js
+++ src/routes/urlsRouter.js
@@ -1,12 +1,12 @@
 import {Router} from 'express';
 import { shortenUrl } from '../controllers/urlsController.js';
 import { checkToken, checkUrlSchema } from '../middlewares/urlsMiddleware.js';
-import {timeout} from 'connect-timeout';
+import timeout from 'connect-timeout';
 
 const urlsRouter = Router();
+const time = timeout(); 
 
-
-urlsRouter.post("/urls/shorten", timeout('5s'), checkToken, checkUrlSchema, shortenUrl);
+urlsRouter.post("/urls/shorten", time('5s'), checkToken, checkUrlSchema, shortenUrl);
 
 
 export default urlsRouter;
